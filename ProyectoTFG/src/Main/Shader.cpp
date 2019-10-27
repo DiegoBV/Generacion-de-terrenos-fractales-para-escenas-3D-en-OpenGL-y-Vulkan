019b2c32bdc715252e47dceecf5a1ce0a2e62d8f
Diego BV <diego.baratto8@gmail.com>
2019-10-27 18:14:27 +0100

    RayMarching
    Co-Authored-By: Jorge Rodríguez García <jorgerodrigar2703@gmail.com>
    Co-Authored-By: gonzsa04 <gonzsa04@users.noreply.github.com>

---
+++ ProyectoTFG/src/Main/Shader.cpp
@@ -0,0 +1,177 @@
+#include "Shader.h"
+#include <glad/glad.h>
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+
+Shader::Shader()
+{
+}
+
+Shader::~Shader()
+{
+}
+
+void Shader::init(const char * vertexPath, const char * fragmentPath)
+{
+	std::string vertexCode = readFile(vertexPath);
+	std::string fragmentCode = readFile(fragmentPath);
+
+	const char* vShaderCode = vertexCode.c_str();
+	const char * fShaderCode = fragmentCode.c_str();
+	// COMPILE SHADERS
+	unsigned int vertex, fragment;
+	// vertex shader
+	vertex = compileShader(GL_VERTEX_SHADER, vShaderCode);
+	checkCompileErrors(vertex, "VERTEX");
+	// fragment Shader
+	fragment = compileShader(GL_FRAGMENT_SHADER, fShaderCode);
+	checkCompileErrors(fragment, "FRAGMENT");
+	// shader Program
+	ID = createGLProgram(vertex, fragment);
+	checkCompileErrors(ID, "PROGRAM");
+
+	// delete the shaders as they're linked into our program now and no longer necessary
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+unsigned int Shader::compileShader(unsigned int type, const char* source)
+{
+	unsigned int shader;
+	shader = glCreateShader(type);
+	glShaderSource(shader, 1, &source, NULL);
+	glCompileShader(shader);
+
+	return shader;
+}
+
+unsigned int Shader::createGLProgram(unsigned int vertexShader, unsigned int fragmentShader)
+{
+	unsigned int programID = glCreateProgram();
+	glAttachShader(programID, vertexShader);
+	glAttachShader(programID, fragmentShader);
+	glLinkProgram(programID);
+
+	return programID;
+}
+
+void Shader::use()
+{
+	glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string & name, bool value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string & name, int value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string & name, float value) const
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setVec2(const std::string & name, const glm::vec2 & value) const
+{
+	glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void Shader::setVec2(const std::string & name, float x, float y) const
+{
+	glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+}
+
+void Shader::setVec3(const std::string & name, const glm::vec3 & value) const
+{
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void Shader::setVec3(const std::string & name, float x, float y, float z) const
+{
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
+
+void Shader::setVec4(const std::string & name, const glm::vec4 & value) const
+{
+	glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void Shader::setVec4(const std::string & name, float x, float y, float z, float w)
+{
+	glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+}
+
+void Shader::setMat2(const std::string & name, const glm::mat2 & mat) const
+{
+	glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::setMat3(const std::string & name, const glm::mat3 & mat) const
+{
+	glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::setMat4(const std::string & name, const glm::mat4 & mat) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::checkCompileErrors(unsigned int shader, std::string type)
+{
+	int success;
+	char infoLog[1024];
+	if (type != "PROGRAM")
+	{
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+	else
+	{
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+}
+
+std::string Shader::readFile(const char* filePath)
+{
+	std::string outputString;
+	std::ifstream inputFile;
+
+	// ensure ifstream objects can throw exceptions:
+	inputFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+	try
+	{
+		// open files
+		inputFile.open(filePath);
+		std::stringstream stream;
+		// read file's buffer contents into streams
+		stream << inputFile.rdbuf();
+		// close file handlers
+		inputFile.close();
+		// convert stream into string
+		outputString = stream.str();
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+
+	return outputString;
+}
--- ProyectoTFG/src/Main/main.cpp
+++ ProyectoTFG/src/Main/main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "Shader.h"
 
 #include <iostream>
 
@@ -43,6 +44,33 @@ int main()
 		return -1;
 	}
 
+	Shader shader = Shader();
+	shader.init("vertex.txt", "fragment.txt");
+	shader.setVec2("resolution", glm::vec2(SCR_WIDTH, SCR_HEIGHT));
+
+	float vertices[] = {
+		// positions         // colors
+		 0.5f, -0.5f, 1.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 1.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		 0.0f,  0.5f, 1.0f,  0.0f, 0.0f, 1.0f   // top 
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -50,7 +78,9 @@ int main()
 		// input
 		// -----
 		processInput(window);
-
+		shader.use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
