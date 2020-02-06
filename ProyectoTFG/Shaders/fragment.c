7cfd292b0a42c3642e894a8dd2bb3ff919647800
Unknown <gonzsa04@ucm.es>
2020-02-06 12:23:40 +0100

    Animacion

--- ProyectoTFG/Shaders/fragment.c
+++ ProyectoTFG/Shaders/fragment.c
@@ -35,30 +35,67 @@ float getDistance(vec3 point){
     sphere.center = vec3(1.0, 0.0, 0.0);
     sphere.radius = 3.0;
     sphere.color = vec4(1.0, 0.0, 0.0, 1.0);
-    Box box;
-    box.center = vec3(0.0, 0.0, 0.0);
-    box.dimensions = vec3(2.5, 2.5, 2.5);
-    box.color = vec4(0.0, 1.0, 0.0, 1.0);
-    box.rotation = mat3(
-        1, 0, 0,
-        0, 1, 0,
-        0, 0, 1
-        );
-    box.rotateZ(30.0);
-    
-    float radiusCylinder = 1.5;
-    float height = 3.0;
 
-    float intersection = intersectSDF(box.SDF(point), sphere.SDF(point));
-    intersection = differenceSDF(intersection, SDFCylinder(point, height, radiusCylinder));
-    return min(intersection, point.y + 5.0);
+    return min(sphere.SDF(point), point.y + 5.0);
+}
+
+float sceneSDF(vec3 point){
+    mat3 identity = mat3(1, 0, 0, 0, 1, 0, 0, 0, 1);
+    point = rotateY(degrees(time / 2.0), identity) * point;
+
+    Plane plane; plane.height = 5.0;
+
+    Sphere sphere; sphere.center = vec3(0.0, 0.0, 0.0); sphere.radius = 2.4; sphere.color = vec4(1.0, 0.0, 0.0, 1.0);
+    sphere.rotation = identity;
+    Box box; box.center = vec3(0.0, 0.0, 0.0); box.dimensions = vec3(2, 2, 2); box.color = vec4(0.0, 1.0, 0.0, 1.0);
+    box.rotation = identity;
+
+    float cylinderRadius = 0.8 + (1.0 - 0.4) * (3.0 + sin(1.7 * time)) / 2.0;
+    Cylinder cylinder1; cylinder1.center = vec3(0.0, 0.0, 0.0); cylinder1.height = 2.4; cylinder1.radius = cylinderRadius;
+    cylinder1.rotation = identity;
+    Cylinder cylinder2; cylinder2.center = vec3(0.0, 0.0, 0.0); cylinder2.height = 2.4; cylinder2.radius = cylinderRadius;
+    cylinder2.rotation = identity;
+    cylinder2.rotateFigX(90.0);
+    Cylinder cylinder3; cylinder3.center = vec3(0.0, 0.0, 0.0); cylinder3.height = 2.4; cylinder3.radius = cylinderRadius;
+    cylinder3.rotation = identity;
+    cylinder3.rotateFigY(90.0);
+
+    float ballOffset = 0.4 + 1.0 + sin(1.7 * time);
+    float ballRadius = 0.6;
+
+    Sphere sphere1; sphere1.center = vec3(-ballOffset, 0.0, 0.0); sphere1.radius = ballRadius; sphere1.color =  vec4(1.0, 0.0, 0.0, 1.0);
+    sphere1.rotation = identity;
+    Sphere sphere2; sphere2.center = vec3(ballOffset, 0.0, 0.0); sphere2.radius = ballRadius; sphere2.color =  vec4(1.0, 0.0, 0.0, 1.0);
+    sphere2.rotation = identity;
+    Sphere sphere3; sphere3.center = vec3(0.0, -ballOffset, 0.0); sphere3.radius = ballRadius; sphere3.color =  vec4(1.0, 0.0, 0.0, 1.0);
+    sphere3.rotation = identity;
+    Sphere sphere4; sphere4.center = vec3(0.0, ballOffset, 0.0); sphere4.radius = ballRadius; sphere4.color =  vec4(1.0, 0.0, 0.0, 1.0);
+    sphere4.rotation = identity;
+    Sphere sphere5; sphere5.center = vec3(0.0, 0.0, -ballOffset); sphere5.radius = ballRadius; sphere5.color =  vec4(1.0, 0.0, 0.0, 1.0);
+    sphere5.rotation = identity;
+    Sphere sphere6; sphere6.center = vec3(0.0, 0.0, ballOffset); sphere6.radius = ballRadius; sphere6.color =  vec4(1.0, 0.0, 0.0, 1.0);
+    sphere6.rotation = identity;
+
+    float balls = sphere1.SDF(point);
+    balls = unionSDF(balls, sphere2.SDF(point));
+    balls = unionSDF(balls, sphere3.SDF(point));
+    balls = unionSDF(balls, sphere4.SDF(point));
+    balls = unionSDF(balls, sphere5.SDF(point));
+    balls = unionSDF(balls, sphere6.SDF(point));
+    
+    float csgNut = differenceSDF(intersectSDF(box.SDF(point), sphere.SDF(point)),
+                         unionSDF(cylinder1.SDF(point), unionSDF(cylinder2.SDF(point), cylinder3.SDF(point))));
+    
+    float intersection =  unionSDF(balls, csgNut);
+
+    return min(intersection, plane.SDF(point));
 }
 
 float rayMarch(vec3 eye, vec3 marchingDirection, float start, float end){
     float depth = start;
 
     for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
-        float dist = getDistance(eye + depth * marchingDirection);
+        float dist = sceneSDF(eye + depth * marchingDirection);
         if (dist < EPSILON) {
 			return depth;
         }
@@ -73,9 +110,9 @@ float rayMarch(vec3 eye, vec3 marchingDirection, float start, float end){
 
 vec3 getNormal(vec3 p){
     return normalize(vec3(
-        getDistance(vec3(p.x + EPSILON, p.y, p.z)) - getDistance(vec3(p.x - EPSILON, p.y, p.z)),
-        getDistance(vec3(p.x, p.y + EPSILON, p.z)) - getDistance(vec3(p.x, p.y - EPSILON, p.z)),
-        getDistance(vec3(p.x, p.y, p.z  + EPSILON)) - getDistance(vec3(p.x, p.y, p.z - EPSILON))
+        sceneSDF(vec3(p.x + EPSILON, p.y, p.z)) - sceneSDF(vec3(p.x - EPSILON, p.y, p.z)),
+        sceneSDF(vec3(p.x, p.y + EPSILON, p.z)) - sceneSDF(vec3(p.x, p.y - EPSILON, p.z)),
+        sceneSDF(vec3(p.x, p.y, p.z  + EPSILON)) - sceneSDF(vec3(p.x, p.y, p.z - EPSILON))
     ));
 }
 
@@ -90,6 +127,53 @@ float getLight(vec3 p){
     float distancia = rayMarch(p + (normal * EPSILON * 2.0), light, MIN_DIST, MAX_DIST);
     if(distancia < length(lightPos - p)) dif *= 0.1;
     return dif;
+}
+
+vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
+                          vec3 lightPos, vec3 lightIntensity) {
+    vec3 N = getNormal(p);
+    vec3 L = normalize(lightPos - p);
+    vec3 V = normalize(eye - p);
+    vec3 R = normalize(reflect(-L, N));
+    
+    float dotLN = dot(L, N);
+    float dotRV = dot(R, V);
+    
+    if (dotLN < 0.0) {
+        // Light not visible from this point on the surface
+        return vec3(0.0, 0.0, 0.0);
+    } 
+    
+    if (dotRV < 0.0) {
+        // Light reflection in opposite direction as viewer, apply only diffuse
+        // component
+        return lightIntensity * (k_d * dotLN);
+    }
+    return lightIntensity * (k_d * dotLN + k_s * pow(dotRV, alpha));
+}
+
+vec3 phongIllumination(vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye) {
+    const vec3 ambientLight = 0.5 * vec3(1.0, 1.0, 1.0);
+    vec3 color = ambientLight * k_a;
+    
+    vec3 light1Pos = vec3(4.0 * sin(time),
+                          2.0,
+                          4.0 * cos(time));
+    vec3 light1Intensity = vec3(0.4, 0.4, 0.4);
+    
+    color += phongContribForLight(k_d, k_s, alpha, p, eye,
+                                  light1Pos,
+                                  light1Intensity);
+    
+    vec3 light2Pos = vec3(2.0 * sin(0.37 * time),
+                          2.0 * cos(0.37 * time),
+                          2.0);
+    vec3 light2Intensity = vec3(0.4, 0.4, 0.4);
+    
+    color += phongContribForLight(k_d, k_s, alpha, p, eye,
+                                  light2Pos,
+                                  light2Intensity);    
+    return color;
 }
 
 void main()
@@ -112,6 +196,14 @@ void main()
 		return;
     }
 
-    vec3 col = vec3(dif);
-    gl_FragColor = vec4(col, 1.0);
+    // Use the surface normal as the ambient color of the material
+    vec3 K_a = (getNormal(p) + vec3(1.0)) / 2.0;
+    vec3 K_d = K_a;
+    vec3 K_s = vec3(1.0, 1.0, 1.0);
+    float shininess = 10.0;
+    
+    vec3 phongColor = phongIllumination(K_a, K_d, K_s, shininess, p, cameraPosition);
+
+    vec3 lightColor = vec3(dif);
+    gl_FragColor = vec4(phongColor * lightColor, 1.0);
 }
--- ProyectoTFG/Shaders/geometries.c
+++ ProyectoTFG/Shaders/geometries.c
@@ -1,10 +1,50 @@
+mat3 rotateX(float angle, mat3 rotation)
+{
+    angle = radians(angle);
+    rotation = mat3(
+        1, 0, 0,
+        0, cos(angle), -sin(angle),
+        0, sin(angle), cos(angle));
+    return rotation;
+}
+mat3 rotateY(float angle, mat3 rotation)
+{
+    angle = radians(angle);
+    rotation = mat3(
+        cos(angle), 0, sin(angle),
+        0, 1, 0,
+        -sin(angle), 0, cos(angle));
+    return rotation;
+}
+mat3 rotateZ(float angle, mat3 rotation)
+{
+    angle = radians(angle);
+    rotation = mat3(
+        cos(angle), -sin(angle), 0,
+        sin(angle), cos(angle), 0,
+        0, 0, 1);
+    return rotation;
+}
+
+
 struct Sphere{
     float radius;
     vec3 center;
+    mat3 rotation;
     vec4 color;
 
     float SDF(vec3 point) {
         return length(point - center) - radius;
+    }
+
+    void rotateFigX(float angle){
+        rotation = rotateX(angle, rotation);
+    }
+    void rotateFigY(float angle){
+        rotation = rotateY(angle, rotation);
+    }
+    void rotateFigZ(float angle){
+        rotation = rotateZ(angle, rotation);
     }
 };
 
@@ -20,29 +60,45 @@ struct Box{
         return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
     }
 
-    void rotateX(float angle){
-        angle = radians(angle);
-        rotation = mat3(
-        1, 0, 0,
-        0, cos(angle), -sin(angle),
-        0, sin(angle), cos(angle)  
-        );
+    void rotateFigX(float angle){
+        rotation = rotateX(angle, rotation);
     }
-    void rotateY(float angle){
-        angle = radians(angle);
-        rotation = mat3(
-        cos(angle), 0, sin(angle),
-        0, 1, 0, 
-        -sin(angle), 0, cos(angle) 
-        );
+    void rotateFigY(float angle){
+        rotation = rotateY(angle, rotation);
     }
-    void rotateZ(float angle){
-        angle = radians(angle);
-        rotation = mat3(
-        cos(angle), -sin(angle), 0,
-        sin(angle), cos(angle), 0, 
-        0, 0, 1
-        );
+    void rotateFigZ(float angle){
+        rotation = rotateZ(angle, rotation);
     }
 };
 
+struct Cylinder{
+    vec3 center;
+    float radius;
+    float height;
+    mat3 rotation;
+    vec4 color;
+
+    float SDF(vec3 point) {
+        point = (point - center) * rotation;
+        vec2 d = abs(vec2(length(point.xy),point.z)) - vec2(radius,height);
+        return min(max(d.x,d.y),0.0) + length(max(d,0.0));
+    }
+
+    void rotateFigX(float angle){
+        rotation = rotateX(angle, rotation);
+    }
+    void rotateFigY(float angle){
+        rotation = rotateY(angle, rotation);
+    }
+    void rotateFigZ(float angle){
+        rotation = rotateZ(angle, rotation);
+    }
+};
+
+struct Plane{
+    float height;
+
+    float SDF(vec3 point){
+        return point.y + height;
+    }
+};
